dce09082c38fcd630126dc98f0685e020c918746
JuMaHawk <jmferrero93@gmail.com>
2024-03-17 18:42:31 -0300

    Desafio 1 finalizado

--- Desafio 1/main.js
+++ Desafio 1/main.js
@@ -29,11 +29,6 @@ class ProductManager {
         }
 
         this.products.push(newProduct)
-
-        // const guardadoDeDatos = async () => {
-        //     await fs.promises.writeFile(this.path, JSON.stringify(this.product))
-        // }
-        // guardadoDeDatos();
     }
 
     getProducts() {
@@ -59,13 +54,12 @@ class ProductManager {
 
 
 // 1) Se creará una instancia de la clase “ProductManager”
-
 const manager = new ProductManager();
 
 
 // 2) Se llamará “getProducts” recién creada la instancia, debe devolver un arreglo vacío []
-
 console.log(manager.getProducts());
+
 
 /**  3) Se llamará al método “addProduct” con los campos:
             title: “producto prueba”
@@ -75,22 +69,21 @@ console.log(manager.getProducts());
             code:”abc123”,
             stock:25
 **/
-
 manager.addProduct("Producto prueba", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25)
+
 
 // 4) El objeto debe agregarse satisfactoriamente con un id generado automáticamente SIN REPETIRSE
-
 manager.addProduct("Producto prueba dos", "Este es un segundo producto prueba", 200, "Sin imagen", "abc456", 25)
 
-// 5) Se llamará el método “getProducts” nuevamente, esta vez debe aparecer el producto recién agregado
 
+// 5) Se llamará el método “getProducts” nuevamente, esta vez debe aparecer el producto recién agregado
 console.log(manager.getProducts())
 
-// 6) Se llamará al método “addProduct” con los mismos campos de arriba, debe arrojar un error porque el código estará repetido.
 
+// 6) Se llamará al método “addProduct” con los mismos campos de arriba, debe arrojar un error porque el código estará repetido.
 manager.addProduct("Producto prueba", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25)
 
-// 7) Se evaluará que getProductById devuelva error si no encuentra el producto o el producto en caso de encontrarlo
 
+// 7) Se evaluará que getProductById devuelva error si no encuentra el producto o el producto en caso de encontrarlo
 manager.getProductById(1)
 manager.getProductById(50)
