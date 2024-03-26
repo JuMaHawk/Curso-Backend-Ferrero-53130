e4c0d39c1413b4e2358c246569b705dd8c2a763d
JuMaHawk <jmferrero93@gmail.com>
2024-03-26 16:45:42 -0300

    Pre entrega 1 en proceso

--- Pre-entrega-1/src/controllers/productManager.js
+++ Pre-entrega-1/src/controllers/productManager.js
@@ -57,7 +57,7 @@ export default class ProductManager {
                 console.log("[]");
             }
         } else {
-            console.log([])
+            console.log("[]")
         }
     }
 
--- Pre-entrega-1/src/routes/products.router.js
+++ Pre-entrega-1/src/routes/products.router.js
@@ -4,6 +4,8 @@ import ProductManager from "../controllers/productManager.js";
 const router = express.Router();
 const manager = new ProductManager("./src/models/productos.json")
 
+
+//RUTA PARA TRAER TODOS LOS PRODUCTOS O SOLO LA CANT INDICADA EN LA QUERY LIMIT.
 router.get("/products", (req, res) => {
     let limit = parseInt(req.query.limit);
     let todosProductos = manager.getProducts();
@@ -15,13 +17,11 @@ router.get("/products", (req, res) => {
         }
     } catch (error) {
         console.error("Tuvimos un problema al querer obtener el producto", error);
-        res.status(500).json({
-            error: "Error interno del servidor"
-        });
+        res.status(500).json({ error: "Error interno del servidor" });
     }
 });
 
-
+//RUTA PARA VER ALGUN PRODUCTO EN PARTICULAR SEGUN SU ID.
 router.get("/products/:id", (req, res) => {
     let id = parseInt(req.params.id);
     let todosProductos = manager.getProducts();
@@ -29,5 +29,18 @@ router.get("/products/:id", (req, res) => {
     if (!productoId) return res.send("El producto no existe");
     res.send(productoId);
 });
+
+
+//RUTA PARA AGREGAR PRODUCTOS AL ARRAY.
+router.post("/products", (req, res) => {
+    const newProduct = req.body
+    console.log(newProduct)
+    try {
+        manager.addProduct(newProduct);
+        res.status(201).json({ message: "Producto agregado correctamente" })
+    } catch (error) {
+        res.status(500).json({ error: "Error interno del servidor" })
+    }
+})
 
 export default router;
